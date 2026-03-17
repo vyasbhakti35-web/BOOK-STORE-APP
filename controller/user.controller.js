c97238aac4a9cd41447cc825c3ad3a43c842088f
vyasbhakti35-web <vyasbhakti35@gmail.com>
2026-03-17 17:22:31 -0400

    Initial backend commit

---
+++ controller/user.controller.js
@@ -0,0 +1,26 @@
+import User from "../model/user.model.js";
+import bcryptjs from "bcryptjs"
+
+
+export const signup=async (req,res)=>{
+    try{
+          const {fullname,email,password}=req.body;
+          console.log(req.body);
+          const user= await User.findOne({email})
+          if(user){
+            return res.status(400).json({message:"User already exists"})
+          }
+          const hashPassword=await bcryptjs.hash(password,10)
+          const createdUser=new User({
+            fullname: fullname,
+            email: email,
+            password: hashPassword,
+          });
+          await createdUser.save();
+          res.status(201).json({message:"User created successfully"})
+    }catch (error){
+        console.log("Error: " + error.message)
+        res.status(500).json({message:"Internal server error"})
+
+    }
+}
---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+import bookRoute from "./route/book.route.js"
+import userRoute from "./route/user.route.js"
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+const URI = process.env.MongoDBURI;
+
+// connect to MongoDB
+mongoose.connect(URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("MongoDB error:", err));
+
+  // defining routes
+  app.use("/book" ,bookRoute)
+  app.use("/user", userRoute)
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
